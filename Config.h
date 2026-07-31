501828fcc2e4f4a904ff3274a2755553deddc81a
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:09:16 +0200

    Create Config.h

---
+++ Config.h
@@ -0,0 +1,13 @@
+//=============================================================================
+// File: Config.h
+// Commit: 1
+//=============================================================================
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <Arduino.h>
+
+static constexpr uint32_t SERIAL_BAUDRATE = 115200;
+
+#endif
