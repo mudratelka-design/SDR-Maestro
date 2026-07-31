eeccb265a774d225db28a92724be2f5caf397e25
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:46:39 +0200

    Update OpenWebRXProfile.cpp

--- OpenWebRXProfile.cpp
+++ OpenWebRXProfile.cpp
@@ -1,10 +1,25 @@
 //=============================================================================
 // File: OpenWebRXProfile.cpp
-// Commit: 1
+// Commit: 3
 //=============================================================================
 
 #include "OpenWebRXProfile.h"
 
 void OpenWebRXProfile::begin()
 {
+}
+
+ActionType OpenWebRXProfile::getClockwiseAction() const
+{
+    return ActionType::FrequencyUp;
+}
+
+ActionType OpenWebRXProfile::getCounterClockwiseAction() const
+{
+    return ActionType::FrequencyDown;
+}
+
+ActionType OpenWebRXProfile::getButtonAction() const
+{
+    return ActionType::ToggleMute;
 }
