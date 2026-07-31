53753c6b97eb22c37974532e572fa33aacf4b907
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:13:05 +0200

    Create ButtonManager.h

---
+++ ButtonManager.h
@@ -0,0 +1,16 @@
+//=============================================================================
+// File: ButtonManager.h
+// Commit: 1
+//=============================================================================
+
+#ifndef BUTTON_MANAGER_H
+#define BUTTON_MANAGER_H
+
+class ButtonManager
+{
+public:
+    void begin();
+    void update();
+};
+
+#endif
