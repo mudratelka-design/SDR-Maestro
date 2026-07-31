ccaab73890e71f461249d10d65ed8154d68ff12b
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:18:17 +0200

    Create StorageManager.h

---
+++ StorageManager.h
@@ -0,0 +1,15 @@
+//=============================================================================
+// File: StorageManager.h
+// Commit: 1
+//=============================================================================
+
+#ifndef STORAGE_MANAGER_H
+#define STORAGE_MANAGER_H
+
+class StorageManager
+{
+public:
+    void begin();
+};
+
+#endif
