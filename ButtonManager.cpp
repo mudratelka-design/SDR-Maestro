11d082ad19faed19df98185fdd6fb95109ad9f85
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:43:41 +0200

    Update ButtonManager.cpp

--- ButtonManager.cpp
+++ ButtonManager.cpp
@@ -1,14 +1,44 @@
 //=============================================================================
 // File: ButtonManager.cpp
-// Commit: 1
+// Commit: 2
 //=============================================================================
 
+#include <Arduino.h>
+
 #include "ButtonManager.h"
+#include "Queue.h"
+#include "Events.h"
+
+#ifndef BUTTON_PIN
+#define BUTTON_PIN 3
+#endif
 
 void ButtonManager::begin()
 {
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+    lastState = digitalRead(BUTTON_PIN);
 }
 
 void ButtonManager::update()
 {
+    bool state = digitalRead(BUTTON_PIN);
+
+    if (state != lastState)
+    {
+        Event event;
+
+        if (!state)
+        {
+            event.type = EventType::ButtonPressed;
+        }
+        else
+        {
+            event.type = EventType::ButtonReleased;
+        }
+
+        EventQueue.push(event);
+
+        lastState = state;
+    }
 }
