d92acd98c10e60ba808401d8ab2b80b1ed8b4903
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:12:43 +0200

    Create EncoderManager.cpp

---
+++ EncoderManager.cpp
@@ -0,0 +1,14 @@
+//=============================================================================
+// File: EncoderManager.cpp
+// Commit: 1
+//=============================================================================
+
+#include "EncoderManager.h"
+
+void EncoderManager::begin()
+{
+}
+
+void EncoderManager::update()
+{
+}
