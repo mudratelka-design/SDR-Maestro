33a8d3b48bba90fbba49714c936443abb126cd68
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:16:26 +0200

    Create ActionManager.cpp

---
+++ ActionManager.cpp
@@ -0,0 +1,14 @@
+//=============================================================================
+// File: ActionManager.cpp
+// Commit: 1
+//=============================================================================
+
+#include "ActionManager.h"
+
+void ActionManager::begin()
+{
+}
+
+void ActionManager::update()
+{
+}
