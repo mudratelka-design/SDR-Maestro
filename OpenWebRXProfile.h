84adb6dbe4c160d04d33108eeaf6c4cc01f58744
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:46:22 +0200

    Update OpenWebRXProfile.h

--- OpenWebRXProfile.h
+++ OpenWebRXProfile.h
@@ -1,15 +1,21 @@
 //=============================================================================
 // File: OpenWebRXProfile.h
-// Commit: 1
+// Commit: 3
 //=============================================================================
 
 #ifndef OPENWEBRX_PROFILE_H
 #define OPENWEBRX_PROFILE_H
 
+#include "Types.h"
+
 class OpenWebRXProfile
 {
 public:
     void begin();
+
+    ActionType getClockwiseAction() const;
+    ActionType getCounterClockwiseAction() const;
+    ActionType getButtonAction() const;
 };
 
 #endif
