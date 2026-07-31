f08322bda97a7bdc7dd37977a492be6556564ea5
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:17:36 +0200

    Create ProfileManager.h

---
+++ ProfileManager.h
@@ -0,0 +1,15 @@
+//=============================================================================
+// File: ProfileManager.h
+// Commit: 1
+//=============================================================================
+
+#ifndef PROFILE_MANAGER_H
+#define PROFILE_MANAGER_H
+
+class ProfileManager
+{
+public:
+    void begin();
+};
+
+#endif
