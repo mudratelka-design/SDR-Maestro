991a523dd9930472aa8801d0f0335b5f330e9408
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:45:51 +0200

    Update ProfileManager.cpp

--- ProfileManager.cpp
+++ ProfileManager.cpp
@@ -1,10 +1,33 @@
 //=============================================================================
 // File: ProfileManager.cpp
-// Commit: 1
+// Commit: 3
 //=============================================================================
 
 #include "ProfileManager.h"
+#include "OpenWebRXProfile.h"
+
+static OpenWebRXProfile Profile;
 
 void ProfileManager::begin()
 {
+    Profile.begin();
+
+    clockwiseAction = Profile.getClockwiseAction();
+    counterClockwiseAction = Profile.getCounterClockwiseAction();
+    buttonAction = Profile.getButtonAction();
+}
+
+ActionType ProfileManager::getClockwiseAction() const
+{
+    return clockwiseAction;
+}
+
+ActionType ProfileManager::getCounterClockwiseAction() const
+{
+    return counterClockwiseAction;
+}
+
+ActionType ProfileManager::getButtonAction() const
+{
+    return buttonAction;
 }
