3761812db91920a8e078988e1374e6f87880bcce
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:16:03 +0200

    Create ActionManager.h

---
+++ ActionManager.h
@@ -0,0 +1,16 @@
+//=============================================================================
+// File: ActionManager.h
+// Commit: 1
+//=============================================================================
+
+#ifndef ACTION_MANAGER_H
+#define ACTION_MANAGER_H
+
+class ActionManager
+{
+public:
+    void begin();
+    void update();
+};
+
+#endif
