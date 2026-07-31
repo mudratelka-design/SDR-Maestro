55eca19e83d577e34816c46f09828e48f8f40028
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:45:23 +0200

    Update ProfileManager.h

--- ProfileManager.h
+++ ProfileManager.h
@@ -1,15 +1,26 @@
 //=============================================================================
 // File: ProfileManager.h
-// Commit: 1
+// Commit: 3
 //=============================================================================
 
 #ifndef PROFILE_MANAGER_H
 #define PROFILE_MANAGER_H
 
+#include "Types.h"
+
 class ProfileManager
 {
 public:
     void begin();
+
+    ActionType getClockwiseAction() const;
+    ActionType getCounterClockwiseAction() const;
+    ActionType getButtonAction() const;
+
+private:
+    ActionType clockwiseAction;
+    ActionType counterClockwiseAction;
+    ActionType buttonAction;
 };
 
 #endif
