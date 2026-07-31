437f16d11309f4d1cd0ee030496bb903c203fc60
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:53:19 +0200

    Update HIDManager.h

--- HIDManager.h
+++ HIDManager.h
@@ -1,5 +1,32 @@
 //=============================================================================
 // File: HIDManager.h
+// Commit: 4 (2/4)
+//=============================================================================
+
+#ifndef HID_MANAGER_H
+#define HID_MANAGER_H
+
+#include <Arduino.h>
+#include "Types.h"
+
+class HIDManager
+{
+public:
+    void begin();
+    void update();
+
+    bool isConnected() const;
+
+    void execute(const Action& action);
+
+private:
+    void executeMouse(const Action& action);
+
+    bool connected = false;
+};
+
+#endif//=============================================================================
+// File: HIDManager.h
 // Commit: 4
 //=============================================================================
 
