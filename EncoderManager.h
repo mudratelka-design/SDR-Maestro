8131c00206a970108ed71f78bfa9f9f7c57454f1
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:12:23 +0200

    Create EncoderManager.h

---
+++ EncoderManager.h
@@ -0,0 +1,16 @@
+//=============================================================================
+// File: EncoderManager.h
+// Commit: 1
+//=============================================================================
+
+#ifndef ENCODER_MANAGER_H
+#define ENCODER_MANAGER_H
+
+class EncoderManager
+{
+public:
+    void begin();
+    void update();
+};
+
+#endif
