99419c04a371db564c3929988f30038595591ea7
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:50:37 +0200

    Update HIDManager.cpp

--- HIDManager.cpp
+++ HIDManager.cpp
@@ -1,14 +1,42 @@
 //=============================================================================
 // File: HIDManager.cpp
-// Commit: 1
+// Commit: 4
 //=============================================================================
 
 #include "HIDManager.h"
 
+#include <BleKeyboard.h>
+
+static BleKeyboard BleHID(
+    "SDR Maestro",
+    "OpenAI",
+    100);
+
 void HIDManager::begin()
 {
+    BleHID.begin();
 }
 
 void HIDManager::update()
 {
+    connected = BleHID.isConnected();
+}
+
+bool HIDManager::isConnected() const
+{
+    return connected;
+}
+
+void HIDManager::execute(const Action& action)
+{
+    (void)action;
+
+    // Commit #4 (2/4)
+    // Mouse support.
+
+    // Commit #4 (3/4)
+    // Keyboard support.
+
+    // Commit #4 (4/4)
+    // Consumer keys support.
 }
