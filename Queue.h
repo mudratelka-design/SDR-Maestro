b56ba0029b727e5b7942b6b3ddd0fd1a7818f9e3
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:39:46 +0200

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,15 +1,34 @@
 //=============================================================================
 // File: Queue.h
-// Commit: 1
+// Commit: 2
 //=============================================================================
 
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "Events.h"
+
 class Queue
 {
 public:
+    static constexpr uint8_t Capacity = 32;
+
     void begin();
+
+    bool push(const Event& event);
+    bool pop(Event& event);
+
+    bool isEmpty() const;
+    bool isFull() const;
+
+private:
+    Event buffer[Capacity];
+
+    volatile uint8_t head = 0;
+    volatile uint8_t tail = 0;
+    volatile uint8_t count = 0;
 };
+
+extern Queue EventQueue;
 
 #endif
