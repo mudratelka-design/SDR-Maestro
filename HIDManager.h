81cd152841238b801e8cc13700c721b50f433f43
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:50:11 +0200

    Update HIDManager.h

--- HIDManager.h
+++ HIDManager.h
@@ -1,16 +1,26 @@
 //=============================================================================
 // File: HIDManager.h
-// Commit: 1
+// Commit: 4
 //=============================================================================
 
 #ifndef HID_MANAGER_H
 #define HID_MANAGER_H
+
+#include <Arduino.h>
+#include "Types.h"
 
 class HIDManager
 {
 public:
     void begin();
     void update();
+
+    bool isConnected() const;
+
+    void execute(const Action& action);
+
+private:
+    bool connected = false;
 };
 
 #endif
