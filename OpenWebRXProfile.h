1e9c585d318a672bb636c586dee812b0ed0e7c11
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:19:11 +0200

    Create OpenWebRXProfile.h

---
+++ OpenWebRXProfile.h
@@ -0,0 +1,15 @@
+//=============================================================================
+// File: OpenWebRXProfile.h
+// Commit: 1
+//=============================================================================
+
+#ifndef OPENWEBRX_PROFILE_H
+#define OPENWEBRX_PROFILE_H
+
+class OpenWebRXProfile
+{
+public:
+    void begin();
+};
+
+#endif
