3e5b40062f727aab1d255fe003e5df7c3cf18fe6
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:16:48 +0200

    Create HIDManager.h

---
+++ HIDManager.h
@@ -0,0 +1,16 @@
+//=============================================================================
+// File: HIDManager.h
+// Commit: 1
+//=============================================================================
+
+#ifndef HID_MANAGER_H
+#define HID_MANAGER_H
+
+class HIDManager
+{
+public:
+    void begin();
+    void update();
+};
+
+#endif
