a683c44ecc22249f218bc78703054df5790605bf
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:47:10 +0200

    Update ActionManager.h

--- ActionManager.h
+++ ActionManager.h
@@ -1,16 +1,22 @@
 //=============================================================================
 // File: ActionManager.h
-// Commit: 1
+// Commit: 3
 //=============================================================================
 
 #ifndef ACTION_MANAGER_H
 #define ACTION_MANAGER_H
+
+#include "Events.h"
+#include "Types.h"
 
 class ActionManager
 {
 public:
     void begin();
     void update();
+
+private:
+    Action mapEventToAction(const Event& event);
 };
 
 #endif
