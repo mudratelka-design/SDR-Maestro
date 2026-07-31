9828e4c32a63763d147f1faa858645073d39023b
mudratelka-design <mudratelka@gmail.com>
2026-07-31 06:09:43 +0200

    Create Version.h

---
+++ Version.h
@@ -0,0 +1,13 @@
+//=============================================================================
+// File: Version.h
+// Commit: 1
+//=============================================================================
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define PROJECT_NAME    "SDR Maestro"
+#define PROJECT_VERSION "0.1.0"
+#define PROJECT_COMMIT  "Commit 1"
+
+#endif
